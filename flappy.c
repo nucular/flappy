df1fb0b86ed76cd90b3e34e623b18eeb1b3d49ad
nucular <nucular@users.noreply.github.com>
2014-09-14 21:33:10 +0200

    Basic program structure

---
+++ flappy.c
@@ -0,0 +1,20 @@
+//#!/usr/bin/env tigcc
+// tigcc -O2 flappy.c
+
+#define SAVE_SCREEN
+#define USE_V200
+#define USE_TI92PLUS
+#define USE_V200
+
+#define COMMENT_STRING "\"Flappy Bird\" clone for TI calculators"
+#define COMMENT_PROGRAM_NAME "flappy"
+#define COMMENT_VERSION_STRING "0.1"
+#define COMMENT_VERSION_NUMBER 0,1,0,0 /* major, minor, revision, subrevision */
+#define COMMENT_AUTHORS "nucular"
+#define MIN_AMS 200
+
+#include <tigcclib.h>
+
+void _main(void)
+{
+}
